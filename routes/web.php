8c6d13b9db45fbf66fd1aeef54cc2aa0461f4e95
andi <aandi30082001@gmail.com>
2020-05-20 16:14:48 +0700

    mengubah routes ke login

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function(){
+    return view('auth.login');
 });
 
 Auth::routes();
+
+Route::match(["GET", "POST"], "/register", function(){
+    return redirect("/login");
+})->name("register");
 
 Route::get('/home', 'HomeController@index')->name('home');
